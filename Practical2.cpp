4679b50509cccb199b0feccff7c979b01d15e235
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-12-26 14:07:07 +0530

    Handled Null String and invalid characters

--- Practical2.cpp
+++ Practical2.cpp
@@ -15,83 +15,222 @@ public:
 
 int main()
 {
-    int nSymbols;
-    cout << "Number of input symbols : ";
-    cin >> nSymbols;
-
-    vector<char> v;
-
-    cout << "Input Symbols : ";
-    for (int i = 0; i < nSymbols; i++)
+    int nSymbols, testCaseNo;
+    cout << "Enter the test case number : ";
+    cin >> testCaseNo;
+    cin.ignore();
+    if (testCaseNo == 3)
     {
-        char temp;
-        cin >> temp;
-        v.push_back(temp);
-    }
+        vector<char> v = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                          'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                          '4', '5', '6', '7', '8', '9'};
+        int initialState = 1, nStates = 3;
+        vector<int> acceptStates = {2};
 
-    int nStates;
-    cout << "Enter number of states : ";
-    cin >> nStates;
+        vector<State> DFA;
 
-    int initialState;
-    cout << "Enter initial state : ";
-    cin >> initialState;
-
-    int nAcceptStates;
-    cout << "Number of Accepting States : ";
-    cin >> nAcceptStates;
-
-    vector<int> acceptStates;
-    cout << "Accepting States : ";
-    for (int i = 0; i < nAcceptStates; i++)
-    {
-        int temp;
-        cin >> temp;
-        acceptStates.push_back(temp);
-    }
-
-    vector<State> DFA;
-    cout << "Transition Table" << endl;
-
-    for (int i = 1; i <= nStates; i++)
-    {
-        vector<pair<char, int>> temp;
-        for (int j = 0; j < nSymbols; j++)
+        for (int i = 0; i < nStates; i++)
         {
-            int transition;
-            cout << i << " to " << v[j] << " -> ";
-            cin >> transition;
-            temp.push_back(make_pair(v[j], transition));
-        }
-        DFA.push_back(State(i, temp));
-    }
-
-    string str;
-    cout << "\nInput String :";
-    cin >> str;
-    int state = initialState;
-    for (int i = 0; i < str.length(); i++)
-    {
-        vector<pair<char, int>> temp = DFA[state - 1].transitions;
-        for (int j = 0; j < temp.size(); j++)
-        {
-            if (temp[j].first == str[i])
+            vector<pair<char, int>> temp;
+            for (int j = 0; j < v.size(); j++)
             {
-                state = temp[j].second;
-                break;
+                if (i == 0)
+                {
+                    if (v[j] >= 'a' && v[j] <= 'z')
+                        temp.push_back(make_pair(v[j], 2));
+                    else
+                        temp.push_back(make_pair(v[j], 3));
+                }
+                else
+                {
+                    temp.push_back(make_pair(v[j], i + 1));
+                }
             }
+            DFA.push_back(State(i + 1, temp));
         }
-    }
 
-    for (int i = 0; i < acceptStates.size(); i++)
-    {
-        if (state == acceptStates[i])
+        string str;
+        while (true)
         {
-            cout << "\nValid String";
-            return 0;
+            cout << "\nInput String (type 'exit' to quit): ";
+
+            getline(cin, str);
+            if (str == "")
+            {
+                cout << "Null String not allowed\n";
+                continue;
+            }
+
+            if (str == "exit")
+                break;
+
+            int state = initialState;
+            bool flag = false;
+            for (int i = 0; i < str.length(); i++)
+            {
+                vector<pair<char, int>> temp = DFA[state - 1].transitions;
+                for (int j = 0; j < temp.size(); j++)
+                {
+                    if (temp[j].first == str[i])
+                    {
+                        state = temp[j].second;
+                        break;
+                    }
+                    if (j == temp.size() - 1)
+                    {
+                        flag = true;
+                    }
+                }
+            }
+
+            if (flag)
+            {
+                cout << "Invalid Character found in the input string\n";
+                continue;
+            }
+            bool isValid = false;
+            for (int i = 0; i < acceptStates.size(); i++)
+            {
+                if (state == acceptStates[i])
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+
+            if (isValid)
+                cout << "Valid String\n";
+            else
+                cout << "Invalid String\n";
         }
     }
+    else
+    {
+        cout << "Number of input symbols : ";
+        cin >> nSymbols;
 
-    cout << "Invalid String";
+        vector<char> v;
+
+        cout << "Input Symbols : ";
+        for (int i = 0; i < nSymbols; i++)
+        {
+            char temp;
+            cin >> temp;
+            v.push_back(temp);
+        }
+
+        int nStates;
+        cout << "Enter number of states : ";
+        cin >> nStates;
+
+        int initialState;
+        cout << "Enter initial state : ";
+        cin >> initialState;
+        while (!(initialState <= nStates && initialState >= 1))
+        {
+            cout << "Invalid Initial State, Enter a valid One\n";
+            cin >> initialState;
+        }
+
+        int nAcceptStates;
+        cout << "Number of Accepting States : ";
+        cin >> nAcceptStates;
+        while (!(nAcceptStates <= nStates && nAcceptStates >= 1))
+        {
+            cout << "Invalid Number of Accepting States, Enter a valid One\n";
+            cin >> nAcceptStates;
+        }
+
+        vector<int> acceptStates;
+        cout << "Accepting States : ";
+        for (int i = 0; i < nAcceptStates; i++)
+        {
+            int temp;
+            cin >> temp;
+            while (temp > nStates || temp < 1)
+            {
+                cout << "Invalid State, Enter a valid One\n";
+                cin >> temp;
+            }
+            acceptStates.push_back(temp);
+        }
+
+        vector<State> DFA;
+        cout << "Transition Table" << endl;
+
+        for (int i = 1; i <= nStates; i++)
+        {
+            vector<pair<char, int>> temp;
+            for (int j = 0; j < nSymbols; j++)
+            {
+                int transition;
+                cout << i << " to " << v[j] << " -> ";
+                cin >> transition;
+                while (transition > nStates || transition < 1)
+                {
+                    cout << "Invalid Transition, Enter a valid One\n";
+                    cout << i << " to " << v[j] << " -> ";
+                    cin >> transition;
+                }
+                temp.push_back(make_pair(v[j], transition));
+            }
+            DFA.push_back(State(i, temp));
+        }
+
+        string str;
+        while (true)
+        {
+            cout << "\nInput String (type 'exit' to quit): ";
+            getline(cin, str);
+            cin.ignore();
+            if (str == "")
+            {
+                cout << "Null String not allowed\n";
+                continue;
+            }
+            if (str == "exit")
+                break;
+
+            int state = initialState;
+            bool flag = false;
+            for (int i = 0; i < str.length(); i++)
+            {
+                vector<pair<char, int>> temp = DFA[state - 1].transitions;
+                for (int j = 0; j < temp.size(); j++)
+                {
+                    if (temp[j].first == str[i])
+                    {
+                        state = temp[j].second;
+                        break;
+                    }
+                    if (j == temp.size() - 1)
+                    {
+                        flag = true;
+                    }
+                }
+                if (flag)
+                {
+                    cout << "Invalid Character found in the input string\n";
+                    continue;
+                }
+            }
+
+            bool isValid = false;
+            for (int i = 0; i < acceptStates.size(); i++)
+            {
+                if (state == acceptStates[i])
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+
+            if (isValid)
+                cout << "Valid String\n";
+            else
+                cout << "Invalid String\n";
+        }
+    }
     return 0;
 }
